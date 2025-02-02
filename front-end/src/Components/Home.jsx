2d1026519b424d329ffdad2e861427784f278461
Origamini <kukavitx@gmail.com>
2025-02-02 13:43:43 +0530

    5th commit

--- front-end/src/Components/Home.jsx
+++ front-end/src/Components/Home.jsx
@@ -33,45 +33,49 @@ const Home = () => {
       <div className="module d-flex">
         <div className="container">
           <h1>Kavitha Kumari</h1>
-          <img src="https://th.bing.com/th/id/R.3c0f2e349d358d3fadf42ec515d8a181?rik=NXueaHqlkRfEjg&riu=http%3a%2f%2fmedia.giphy.com%2fmedia%2fnXJpLaJsYJzCE%2fgiphy.gif&ehk=M5qEmPvd%2fgivmDWxWRPCUjdBN0BNFMDMmWg4mEA2wiE%3d&risl=&pid=ImgRaw&r=0" alt="an error" />
           <p>
             I a web developer with knowledge in HTML, CSS, Javascript, ReactJs and know programming languages like Java, Python worked with apis and know cloud technology AWS.
           </p>
         </div>
       </div>
       <div className=" module contact-details pb-5">
-        <h1>Would like to message me!</h1>
+        <h1 style={{textAlign:'center',marginBottom:'50px'}}>Would like to message me!</h1>
         <form>
-          <div className="mb-3">
-            <label htmlFor="name" className="form-label">
+          <div className="mb-3 d-flex align-items-center">
+            {/* <label htmlFor="name" className="form-label">
               Name
-            </label>
+            </label> */}
             <input
               type="text"
               id="name"
               name="name"
               value={feed.name}
-              className="form-control"
+              className="input"
               onChange={onChange}
+              placeholder='name'
             />
+            {/* <i class='bx bx-user-circle bx-sm'></i> */}
+            <img src="https://th.bing.com/th/id/R.3c0f2e349d358d3fadf42ec515d8a181?rik=NXueaHqlkRfEjg&riu=http%3a%2f%2fmedia.giphy.com%2fmedia%2fnXJpLaJsYJzCE%2fgiphy.gif&ehk=M5qEmPvd%2fgivmDWxWRPCUjdBN0BNFMDMmWg4mEA2wiE%3d&risl=&pid=ImgRaw&r=0" alt="an error" style={{height:'40px'}}/>
           </div>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
+          <div className=" mb-3 d-flex align-items-center">
+            {/* <label htmlFor="email" className="form-label">
               Email
-            </label>
+            </label> */}
             <input
               type="email"
               id="email"
               name="email"
               value={feed.email}
-              className="form-control"
+              className="input"
               onChange={onChange}
+              placeholder='email'
             />
+            {/* <i class='bx bx-envelope bx-sm' ></i> */}
+            <img src="https://s3.getstickerpack.com/storage/uploads/sticker-pack/la-bi-xiao-xin1/sticker_24.gif?a51f9a182994571b5efbadbca0d46f67" alt="an error" style={{height:'40px'}}/>
           </div>
           <div className="form-floating mb-3">
-          <label htmlFor="floatingTextarea">Comments</label>
             <textarea
-              className="form-control"
+              className="textarea"
               placeholder="Leave a comment here"
               id="floatingTextarea"
               name="feedback"
@@ -81,7 +85,7 @@ const Home = () => {
           </div>
           <button
             type="submit"
-            className="btn btn-primary mb-3"
+            className="button"
             onClick={handleClick}
           >
             Submit
