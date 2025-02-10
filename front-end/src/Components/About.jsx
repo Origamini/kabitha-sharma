94c2464bc370b3e166f2a1065e72cfcb4badfac3
Origamini <kukavitx@gmail.com>
2025-02-10 15:02:45 +0530

    seventh commit

--- front-end/src/Components/About.jsx
+++ front-end/src/Components/About.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import {motion} from "framer-motion";
 import './Home.css';
+import './About.css';
 
 const About = () => {
   return (
     <motion.div initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}} className='container'>
-      <div className="module d-flex">This is about</div>
+      {/* <div className="module d-flex">
+        <div className="about_container">
+          <h2>About me!</h2>
+          <small>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam vero et ab magni ad? Est, commodi ad numquam beatae repudiandae asperiores possimus doloremque quod, minus doloribus suscipit libero sequi rerum.</small>
+        </div>
+      </div> */}
+      <div className="module skills">
+        <h2>HTML</h2>
+        <div className="progress_bar">
+          <div className="html"><span>60%</span></div>
+        </div>
+        <h2>CSS</h2>
+        <div className="progress_bar">
+          <div className="css"><span>70%</span></div>
+        </div>
+        <h2>Javascript</h2>
+        <div className="progress_bar">
+          <div className="javascript"><span>40%</span></div>
+        </div>
+        <h2>ReactJs</h2>
+        <div className="progress_bar">
+          <div className="reactjs"><span>70%</span></div>
+        </div>
+      </div>
     </motion.div>
   )
 }
