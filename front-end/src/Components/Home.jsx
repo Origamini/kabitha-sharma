18fb390ebf24136f0bf01f1268f628280747d5f0
Origamini <kukavitx@gmail.com>
2025-03-04 15:07:09 +0530

    ninth commit

--- front-end/src/Components/Home.jsx
+++ front-end/src/Components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Home.css';
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
 const Home = () => {
 
@@ -29,22 +29,19 @@ const Home = () => {
   };
 
   return (
-    <motion.div className="container text-dark" initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
-      <div className="module d-flex">
-        <div className="container">
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className='container'>
+      <div className="module">
+        <div>
           <h1>Kavitha Kumari</h1>
           <p>
             I a web developer with knowledge in HTML, CSS, Javascript, ReactJs and know programming languages like Java, Python worked with apis and know cloud technology AWS.
           </p>
         </div>
       </div>
-      <div className=" module contact-details pb-5">
-        <h1 style={{textAlign:'center',marginBottom:'50px'}}>Would like to message me!</h1>
+      <div className="module">
+        <h1>Would like to message me!</h1>
         <form>
-          <div className="mb-3 d-flex align-items-center">
-            {/* <label htmlFor="name" className="form-label">
-              Name
-            </label> */}
+          <div>
             <input
               type="text"
               id="name"
@@ -54,13 +51,8 @@ const Home = () => {
               onChange={onChange}
               placeholder='name'
             />
-            {/* <i class='bx bx-user-circle bx-sm'></i> */}
-            <img src="https://th.bing.com/th/id/R.3c0f2e349d358d3fadf42ec515d8a181?rik=NXueaHqlkRfEjg&riu=http%3a%2f%2fmedia.giphy.com%2fmedia%2fnXJpLaJsYJzCE%2fgiphy.gif&ehk=M5qEmPvd%2fgivmDWxWRPCUjdBN0BNFMDMmWg4mEA2wiE%3d&risl=&pid=ImgRaw&r=0" alt="an error" style={{height:'40px'}}/>
           </div>
-          <div className=" mb-3 d-flex align-items-center">
-            {/* <label htmlFor="email" className="form-label">
-              Email
-            </label> */}
+          <div>
             <input
               type="email"
               id="email"
@@ -70,14 +62,11 @@ const Home = () => {
               onChange={onChange}
               placeholder='email'
             />
-            {/* <i class='bx bx-envelope bx-sm' ></i> */}
-            <img src="https://s3.getstickerpack.com/storage/uploads/sticker-pack/la-bi-xiao-xin1/sticker_24.gif?a51f9a182994571b5efbadbca0d46f67" alt="an error" style={{height:'40px'}}/>
           </div>
-          <div className="form-floating mb-3">
+          <div>
             <textarea
               className="textarea"
               placeholder="Leave a comment here"
-              id="floatingTextarea"
               name="feedback"
               value={feed.feedback}
               onChange={onChange}
--- front-end/src/Components/Navbar.jsx
+++ front-end/src/Components/Navbar.jsx
@@ -4,43 +4,21 @@ import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark  sticky-top">
-      <Link className="navbar-brand" to="/">Portfolio</Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto">
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/">
-              Home
-            </NavLink>
+    <nav>
+      <NavLink className="portfolio" to="/">Portfolio</NavLink>
+      <div className='links'>
+          <li>
+            <NavLink to="/">Home</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="/about">
-              About
-            </NavLink>
+            <NavLink to="/about">About</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="/experience">
-              Experience
-            </NavLink>
+            <NavLink  to="/experience">Experience</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="/image">
-              Image
-            </NavLink>
+            <NavLink to="/image">Image</NavLink>
           </li>
-        </ul>
       </div>
     </nav>
   );
