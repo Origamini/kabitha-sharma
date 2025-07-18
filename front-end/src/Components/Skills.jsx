5db8ce38ec889b13d75633bfb7347b876fb2cc92
Origamini <kukavitx@gmail.com>
2025-07-18 16:13:46 +0530

    19th commit

--- front-end/src/Components/Skills.jsx
+++ front-end/src/Components/Skills.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { motion } from "framer-motion";
 import './Skills.css';
 
+
 const Skills = () => {
   return (
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
