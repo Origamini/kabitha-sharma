1ba0f44306f7c7dcbcadaba9cc245e210ad72499
Origamini <kukavitx@gmail.com>
2025-07-19 17:50:00 +0530

    20th commit

--- front-end/src/Components/About.jsx
+++ front-end/src/Components/About.jsx
@@ -9,12 +9,26 @@ const About = () => {
         <div className='about'>
           <div className='about-me'>
             <h2>About me!</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam vero et ab magni ad? Est, commodi ad numquam beatae repudiandae asperiores possimus doloremque quod, minus doloribus suscipit libero sequi rerum.</p>
+            <p>Currently, working in Amazon. I am skilled in HTMl, Css, Javascript, NodeJs, ReactJs. Programming languages like Java. Cloud Technology such as AWS. And Devops tools like Jenkins, Docker, Ansible, Kubernetes. Databases like MYSQL.</p>
           </div>
         </div>
         <div className='about-journey'>
           <h2>This is my journey</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, quae eos dolore deleniti, sunt incidunt sit, vero optio quibusdam corrupti blanditiis eaque numquam error accusantium delectus voluptatum doloribus suscipit dolorem?</p>
+          <div className="slider">
+            <div className="slide-track">
+              <div className="slide">Card1</div>
+              <div className="slide ">Card2</div>
+              <div className="slide">Card3</div>
+              <div className="slide">Card4</div>
+              <div className="slide">Card5</div>
+              {/* repeat */}
+              <div className="slide">Card6</div>
+              <div className="slide">Card7</div>
+              <div className="slide">Card8</div>
+              <div className="slide">Card9</div>
+              <div className="slide">Card10</div>
+            </div>
+          </div>
         </div>
       </div>
     </motion.div>
