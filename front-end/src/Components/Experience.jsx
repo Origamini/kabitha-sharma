eec653b639aab5bf2780e26815ca7568b41b379a
Origamini <kukavitx@gmail.com>
2025-09-29 11:59:04 +0530

    24.0

--- front-end/src/Components/Experience.jsx
+++ front-end/src/Components/Experience.jsx
@@ -8,8 +8,21 @@ const Experience = () => {
         <div className='exp-main'>
           <div className="exp">
             <div className="exp-my">
-            <h2>Hey there!</h2>
-            <p>I am working in Amazon as a customer service asscociate. I have also done live projects as a intern in coding cult. I have worked on dockerfiles, kubernetes and terraform. During my phase in AWS Master's course in Intellipaat I did hands on projects on Ansible, Jenkins, Terraform, Linux, Databases and Autoscaling.</p>
+            <h2>Experience</h2>
+            <ul>
+              <li><strong>2022-August:</strong> Completed graduation in Computer Science with the CGPA 9.7.</li>
+              <li>
+                <strong>2022-Present:</strong> Working at Amazon as a Customer Service Associate.
+              </li>
+              <li>
+                <strong>2023-2024:</strong> Completed AWS Master's course from Intellipaat.
+              </li>
+              <li><strong>2025:</strong> Obtained certification in AWS SSA, Developer, SysOps via Credly.com.</li>
+              <li>
+                <strong>2025-June:</strong> Worked as a Intern at Coding culd on live projects on Docker, Kubernetes and Terraform.
+              </li>
+              <li><strong>2025:</strong> Obtained Certification on Basic Java, Css, Javascript via Hackerrank platform.</li>
+            </ul>
             </div>
           </div>
           </div>
